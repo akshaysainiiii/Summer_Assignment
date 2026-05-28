c988302dd002f12a7362462b11b1bb4caf05b4f1
akshaysainiiii <akshaysaini6460594@gmail.com>
2026-05-28 22:19:11 +0530

    FIRST DAY COMPLETED

---
+++ 03_factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Write a program to find factorial of a number 
+
+int main()
+{
+
+    int n;
+    cout << "Enter the number : ";
+    cin >> n;
+
+    int fact = 1;
+    for (int i = n; i >= 1; i--)
+    {
+        fact *= i;
+    }
+
+    cout << "Factorial = " << fact;
+
+    return 0;
+}
